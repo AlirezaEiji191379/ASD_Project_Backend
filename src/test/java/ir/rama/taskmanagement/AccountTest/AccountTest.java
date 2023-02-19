2093cf40317b305a756c7888a9623f02ed0c9a84
alirezaeiji <alirezaeiji151479@gmail.com>
2023-02-19 23:25:02 +0330

    added fake test for checking ci pipeline

---
+++ src/test/java/ir/rama/taskmanagement/AccountTest/AccountTest.java
@@ -0,0 +1,13 @@
+package ir.rama.taskmanagement.AccountTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+public class AccountTest {
+
+    @Test void FakeTest(){
+        boolean a = true;
+        Assert.isTrue(a, "sampletest");
+    }
+
+}
