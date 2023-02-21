9cf1d86518e0d62bee7b96a38f1b3e9acdd2824f
alirezaeiji <alirezaeiji151479@gmail.com>
2023-02-21 22:56:05 +0330

    correct test files

--- src/test/java/ir/rama/taskmanagement/AccountTest/AccountTest.java
+++ src/test/java/ir/rama/taskmanagement/AccountTest/AccountTest.java
@@ -1,19 +1,20 @@
 package ir.rama.taskmanagement.AccountTest;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AccountTest {
 
-    @Test void FakeTest(){
+    @Test
+    public void FakeTest(){
         //System.out.println("fake test was tested");
         boolean a = true;
-        Assert.isTrue(a, "sampletest");
+        Assert.assertEquals(a,true);
     }
 
 }
--- src/test/java/ir/rama/taskmanagement/TaskManagementApplicationTests.java
+++ src/test/java/ir/rama/taskmanagement/TaskManagementApplicationTests.java
@@ -1,6 +1,4 @@
 package ir.rama.taskmanagement;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
