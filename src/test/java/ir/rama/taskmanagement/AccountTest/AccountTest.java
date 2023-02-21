abcb0b9cf6000ef643a49184a2215f1bd7c07f3c
alirezaeiji <alirezaeiji151479@gmail.com>
2023-02-21 21:57:42 +0330

    add test

--- src/test/java/ir/rama/taskmanagement/AccountTest/AccountTest.java
+++ src/test/java/ir/rama/taskmanagement/AccountTest/AccountTest.java
@@ -6,6 +6,7 @@ import org.springframework.util.Assert;
 public class AccountTest {
 
     @Test void FakeTest(){
+        System.out.println("fake test was tested");
         boolean a = true;
         Assert.isTrue(a, "sampletest");
     }
