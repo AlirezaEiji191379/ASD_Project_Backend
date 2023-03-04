94caf90e2e9edde469ea1ed32d2670ad9d91741b
Alireza Eiji <AlirezaEiji151379@gmail.com>
2023-03-04 13:22:16 +0330

    Update TaskManagementApplication.java

--- src/main/java/ir/rama/taskmanagement/TaskManagementApplication.java
+++ src/main/java/ir/rama/taskmanagement/TaskManagementApplication.java
@@ -17,6 +17,6 @@ public class TaskManagementApplication {
     @GetMapping("/")
     public String SayHi(){
         System.out.println("hi controller was hied");
-        return "hello1";
+        return "hello12";
     }
 }
