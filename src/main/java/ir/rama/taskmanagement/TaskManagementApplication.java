6e36e5f45f8b8a0684375dfdc056e8cfea8582e9
alirezaeiji <alirezaeiji151479@gmail.com>
2023-03-07 09:27:03 +0330

    add health check

--- src/main/java/ir/rama/taskmanagement/TaskManagementApplication.java
+++ src/main/java/ir/rama/taskmanagement/TaskManagementApplication.java
@@ -3,11 +3,21 @@ package ir.rama.taskmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
+@RestController
+@RequestMapping("/health")
 public class TaskManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskManagementApplication.class, args);
+    }
+
+    @GetMapping("/check")
+    public String CheckHealthCheck(){
+        return "everything is ok!";
     }
 }
